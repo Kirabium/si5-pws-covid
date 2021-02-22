3a89a1737a8f15c4ba41909f8dad4e85ea94a2de
GraulierBrice <43206351+GraulierBrice@users.noreply.github.com>
2021-02-22 16:01:13 +0100

    New Plague theme colours

--- front/src/pages/dashboard/components/am4chartMap/am4chartMap.js
+++ front/src/pages/dashboard/components/am4chartMap/am4chartMap.js
@@ -7,8 +7,27 @@ import am4geodata_usaHigh from "@amcharts/amcharts4-geodata/usaHigh";
 
 import AnimateNumber from 'react-animated-number';
 import s from './am4chartMap.module.scss';
+
   
   class Am4chartMap extends Component {
+
+    colors = {
+      dark: {
+        lightFill: "#C7D0FF",
+        darkFill:"#474D84",
+        darkerFill:"#354D84",
+        stroke: "#6979C9",
+        pin: "https://i.ibb.co/10ntngd/darkpin.png"
+      },
+      plague: {
+        lightFill: "#E15550",
+        darkFill:"#BF3127",
+        darkerFill:"#94372C",
+        stroke: "#F1FDFA",
+        pin: "https://i.ibb.co/J7MjDzR/plaguepin.png",
+      }
+    }
+    mode = "dark"
   
   componentDidMount() {
     let map = am4core.create("map", am4maps.MapChart);
@@ -25,14 +44,14 @@ import s from './am4chartMap.module.scss';
     map.zoomControl.valign = 'bottom';
     map.zoomControl.dy = -10;
     map.zoomControl.contentHeight = 20;
-    map.zoomControl.minusButton.background.fill = am4core.color("#C7D0FF");
-    map.zoomControl.minusButton.background.stroke = am4core.color("#6979C9");
+    map.zoomControl.minusButton.background.fill = am4core.color(this.colors[this.mode].lightFill);
+    map.zoomControl.minusButton.background.stroke = am4core.color(this.colors[this.mode].stroke);
     map.zoomControl.minusButton.label.fontWeight = 600;
     map.zoomControl.minusButton.label.fontSize = 22;
     map.zoomControl.minusButton.scale = .75;
     map.zoomControl.minusButton.label.scale = .75;
-    map.zoomControl.plusButton.background.fill = am4core.color("#C7D0FF");
-    map.zoomControl.plusButton.background.stroke = am4core.color("#6979C9");
+    map.zoomControl.plusButton.background.fill = am4core.color(this.colors[this.mode].lightFill);
+    map.zoomControl.plusButton.background.stroke = am4core.color(this.colors[this.mode].stroke);
     map.zoomControl.plusButton.label.fontWeight = 600;
     map.zoomControl.plusButton.label.fontSize = 22;
     map.zoomControl.plusButton.label.align = "center";
@@ -40,15 +59,15 @@ import s from './am4chartMap.module.scss';
     map.zoomControl.plusButton.label.scale = .75;
     map.zoomControl.plusButton.dx = 5;
     let plusButtonHoverState = map.zoomControl.plusButton.background.states.create("hover");
-    plusButtonHoverState.properties.fill = am4core.color("#354D84");
+    plusButtonHoverState.properties.fill = am4core.color(this.colors[this.mode].darkFill);
     let minusButtonHoverState = map.zoomControl.minusButton.background.states.create("hover");
-    minusButtonHoverState.properties.fill = am4core.color("#354D84");
+    minusButtonHoverState.properties.fill = am4core.color(this.colors[this.mode].darkFill);
     let polygonTemplate = polygonSeries.mapPolygons.template;
     polygonTemplate.tooltipText = "{name}";
-    polygonTemplate.fill = am4core.color("#474D84");
-    polygonTemplate.stroke = am4core.color("#6979C9")
+    polygonTemplate.fill = am4core.color(this.colors[this.mode].darkFill);
+    polygonTemplate.stroke = am4core.color(this.colors[this.mode].stroke)
     let hs = polygonTemplate.states.create("hover");
-    hs.properties.fill = am4core.color("#354D84");
+    hs.properties.fill = am4core.color(this.colors[this.mode].darkerFill);
     let citySeries = map.series.push(new am4maps.MapImageSeries());
     citySeries.data = cities;
     citySeries.dataFields.value = "size";
@@ -56,13 +75,15 @@ import s from './am4chartMap.module.scss';
     city.nonScaling = true;
     city.propertyFields.latitude = "latitude";
     city.propertyFields.longitude = "longitude";
-    let circle = city.createChild(am4core.Circle);
-    circle.fill = am4core.color("#C7D0FF");
-    circle.strokeWidth = 0;
+    let circle = city.createChild(am4core.Image);
+    //https://i.ibb.co/J7MjDzR/plaguepin.png
+    circle.href = this.colors[this.mode].pin;
+    circle.horizontalCenter = "middle";
+    circle.verticalCenter = "bottom";
+    circle.fill = am4core.color(this.colors[this.mode].darkFill);
     let circleHoverState = circle.states.create("hover");
     circleHoverState.properties.strokeWidth = 1;
     circle.tooltipText = '{tooltip}';
-    circle.propertyFields.radius = 'size';
     this.map = map;
   }
 
