4bbd23639f99070e35abd6c2b2d284a88f0f6833
Virgile <virgile-giovanni.fantauzzi@etu.unice.fr>
2021-02-27 20:01:19 +0100

    add uri to backend

--- back/src/api/mockDB/mockDB.controller.ts
+++ back/src/api/mockDB/mockDB.controller.ts
@@ -3,6 +3,9 @@ import {IIncidenceDayModel, IncidenceDayModel} from "../../lib/incidence_day.mod
 import {HospitalDayModel, IHospitalDayModel} from "../../lib/hospital_day.model";
 //import * as dep from "../assets/dep.json"
 //import * as hospital from "../assets/hospital.json"
+import axios from 'axios';
+
+let incidenceHebdoURI = "https://raw.githubusercontent.com/Kirabium/si5-pws-covid/main/json_files/incidence_dep_hebdo.json";
 
 export class MockDBController {
 
@@ -14,10 +17,12 @@ export class MockDBController {
     }
 
     public async initDB(req: Request, res: Response) {
+        let incidenceHebdoResult = await axios.get(incidenceHebdoURI);
+        let incidenceHebdo = incidenceHebdoResult.data;
         await IncidenceDayModel.deleteMany({});
         await HospitalDayModel.deleteMany({});
         console.log("Collections flushed");
-      //  await IncidenceDayModel.insertMany(dep.default);
+        await IncidenceDayModel.insertMany(incidenceHebdo);
       //  await HospitalDayModel.insertMany(hospital.default);
 
         res.status(200).json("Collections initialized");
