a2ea76c59669ed3fd39eef587c460ce4d1216643
Aldric-Ducreux <neogeekmo@live.fr>
2021-02-28 03:30:30 +0100

    add modal dark mode

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -2,32 +2,26 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router';
 import { HashRouter } from 'react-router-dom';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import axios from 'axios'
 
 /* eslint-disable */
 import ErrorPage from '../pages/error';
 /* eslint-enable */
-
+import LayoutComponent from '../components/Layout';
+import LocationContext from '../contexts/LocationContext'
 import useLocalStorage from '../actions/useLocalStorage';
 
 import '../styles/theme.scss';
-import LayoutComponent from '../components/Layout';
-import LocationContext from '../contexts/LocationContext'
-import axios from 'axios'
 
-const PrivateRoute = ({component, ...rest }) => {
-
-    return ( // eslint-disable-line
-        <Route {...rest} render={props => (React.createElement(component, props))}/>
-    );
-    
-};
 
 function App(props) {
-
-
     const [LatLng, setLatLng] = useState({lat: null, lng: null});
     const [location, setLocation] = useState('UNKNOWN')
     const [storageMode, setStorageMode] = useLocalStorage('theme','plague');
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
 
     const handleChangeMode = useCallback(
         (e) => {
@@ -48,7 +42,7 @@ function App(props) {
                 axios.get(`https://api-adresse.data.gouv.fr/reverse/?lon=${lng}&lat=${lat}`).then(data => {
                     const info = data.data.features;
                     if(info.length > 0) {
-                        setLocation(`${info[0].properties.postcode}`)
+                        setLocation(`${info[0].properties.postcode.substring(0,2)}`)
                     } else {
                        setLocation('Unkwonw')
                     }
@@ -61,27 +55,39 @@ function App(props) {
             })
         }
     }
+    const handleSwicthDarkMode=()=>{
+        handleChangeMode('dark')
+        toggle()
+        return 
+    }
 
     useEffect(()=>{
+        findUserLocation()
+
         document.documentElement.style.setProperty('--current-gradient', `var(--${storageMode}-gradient)`)
         document.documentElement.style.setProperty('--bg-current',`var(--bg-custom-${storageMode})`)
         if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-            handleChangeMode('dark')
+            toggle()
         }
     },[])
-
-    useEffect(() => {
-        findUserLocation()
-    }, [])
-
     
     return (
         <LocationContext.Provider value={ {LatLng, location, setLatLng, setLocation} } >
+            {modal? <Modal isOpen={modal} toggle={toggle}>
+                    <ModalHeader toggle={toggle}>Dark Mode detection</ModalHeader>
+                    <ModalBody>
+                        We have detected the use of a dark mode. Do you want to switch the site to dark mode?
+                    </ModalBody>
+                    <ModalFooter>
+                    <Button color="primary" onClick={handleSwicthDarkMode}>Yes</Button>
+                    <Button color="danger" onClick={toggle}>No, keep this theme</Button>
+                    </ModalFooter>
+                </Modal>: null}
             <div>
                 <HashRouter>
                     <Switch>
                         <Route path="/" exact render={() => <Redirect to="/app"/>}/>
-                        <PrivateRoute path="/app" component={()=><LayoutComponent mode={storageMode} onchange={handleChangeMode}/>}/>
+                        <Route path="/app" component={()=><LayoutComponent mode={storageMode} onchange={handleChangeMode}/>}/>
                         <Route path="/error" exact component={ErrorPage}/>
                         <Route component={ErrorPage}/>
                         <Redirect from="*" to="/app/dashboard"/>
