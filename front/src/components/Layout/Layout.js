5ccc6c9b42338f4c153f301f6e89aec5486e715b
Kirabium <virgile.fantauzzi@hotmail.fr>
2021-02-28 17:00:10 +0100

    add form contact

--- front/src/components/Layout/Layout.js
+++ front/src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Switch, Route, withRouter, Redirect } from 'react-router';
+import {connect} from 'react-redux';
+import {Switch, Route, withRouter, Redirect} from 'react-router';
 
 import Charts from '../../pages/charts_exemple/Charts';
 import Dashboard from '../../pages/dashboard';
@@ -11,54 +11,59 @@ import Sidebar from '../Sidebar';
 import s from './Layout.module.scss';
 import IncidenceList from "../../pages/incidence_list";
 import HospitalList from "../../pages/hospital_list";
+import ContactForm from "../../pages/contact_form/ContactForm";
 
 class Layout extends React.Component {
-  static propTypes = {
-    sidebarStatic: PropTypes.bool,
-    sidebarOpened: PropTypes.bool,
-    mode: PropTypes.string,
-  };
+    static propTypes = {
+        sidebarStatic: PropTypes.bool,
+        sidebarOpened: PropTypes.bool,
+        mode: PropTypes.string,
+    };
 
-  static defaultProps = {
-    sidebarStatic: false,
-    sidebarOpened: false,
-  };
+    static defaultProps = {
+        sidebarStatic: false,
+        sidebarOpened: false,
+    };
 
-  render() {
-    return (
-      <div
-        className={[
-          s.root,
-          'sidebar-' + this.props.sidebarPosition,
-          'sidebar-' + this.props.sidebarVisibility,
-        ].join(' ')}
-      >
-        <div className={s.wrap}>
-          <Header onchange={this.props.onchange} mode={this.props.mode}/>
-          <Sidebar />
-            <main className={s.content}>
-                  <Switch>
-                    <Route path="/app" exact render={() => <Redirect to="/app/dashboard" />} />
-                    <Route path="/app/dashboard" exact component={()=><Dashboard mode={this.props.mode} localisation={this.props.localisation}/>} />
-                    <Route path="/app/hospital/list" exact component={()=><HospitalList mode={this.props.mode} />} />
-                    <Route path="/app/incidence/list" exact component={()=><IncidenceList mode={this.props.mode} />} />
-                  </Switch>
-              <footer className={s.contentFooter}>
-                Dashboard de visualisation des données du covid du groupe DFGH
-              </footer>
-            </main>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div
+                className={[
+                    s.root,
+                    'sidebar-' + this.props.sidebarPosition,
+                    'sidebar-' + this.props.sidebarVisibility,
+                ].join(' ')}
+            >
+                <div className={s.wrap}>
+                    <Header onchange={this.props.onchange} mode={this.props.mode}/>
+                    <Sidebar/>
+                    <main className={s.content}>
+                        <Switch>
+                            <Route path="/app" exact render={() => <Redirect to="/app/dashboard"/>}/>
+                            <Route path="/app/dashboard" exact component={() => <Dashboard mode={this.props.mode}
+                                                                                           localisation={this.props.localisation}/>}/>
+                            <Route path="/app/hospital/list" exact
+                                   component={() => <HospitalList mode={this.props.mode}/>}/>
+                            <Route path="/app/incidence/list" exact
+                                   component={() => <IncidenceList mode={this.props.mode}/>}/>
+                            <Route path="/app/contact" exact component={() => <ContactForm mode={this.props.mode}/>}/>
+                        </Switch>
+                        <footer className={s.contentFooter}>
+                            Dashboard de visualisation des données du covid du groupe DFGH
+                        </footer>
+                    </main>
+                </div>
+            </div>
+        );
+    }
 }
 
 function mapStateToProps(store) {
-  return {
-    sidebarOpened: store.navigation.sidebarOpened,
-    sidebarPosition: store.navigation.sidebarPosition,
-    sidebarVisibility: store.navigation.sidebarVisibility,
-  };
+    return {
+        sidebarOpened: store.navigation.sidebarOpened,
+        sidebarPosition: store.navigation.sidebarPosition,
+        sidebarVisibility: store.navigation.sidebarVisibility,
+    };
 }
 
 export default withRouter(connect(mapStateToProps)(Layout));
