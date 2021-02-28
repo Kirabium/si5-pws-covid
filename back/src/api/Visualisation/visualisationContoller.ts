eb6dd0f34797a7cf4cde68e6f83ee88c7e4a8652
Aldric-Ducreux <neogeekmo@live.fr>
2021-02-28 19:23:14 +0100

    type fixe filtre france

--- back/src/api/Visualisation/visualisationContoller.ts
+++ back/src/api/Visualisation/visualisationContoller.ts
@@ -11,7 +11,7 @@ export class VisualisationContoller {
         const  CasDepByDate= await IncidenceDayDepModel.find({jour: {$regex: "^"+req.params.year+"-"+req.params.month+"-"+req.params.day}, dep: req.params.dep, cl_age90 :Number(req.params.age)});
         const  HospitalByDepAndDate = await HospitalDayModel.find({jour: {$regex: "^"+req.params.year+"-"+req.params.month+"-"+req.params.day}, dep: req.params.dep});
         //donut
-        const listCasFranceByAge= await IncidenceDayDepModel.find({jour: {$regex: "^"+req.params.year+"-"+req.params.month+"-"+req.params.day}});
+        const listCasFranceByAge= await IncidenceDayFranceModel.find({jour: {$regex: "^"+req.params.year+"-"+req.params.month+"-"+req.params.day}});
         
         let jsonRes = {
             casFranceByDate: casFranceByDate,
