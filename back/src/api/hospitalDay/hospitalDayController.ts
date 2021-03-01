1629c136434bd7f4e6c3e55404da3b9f8efd6a14
GraulierBrice <43206351+GraulierBrice@users.noreply.github.com>
2021-03-01 13:19:45 +0100

    Fixed sexe

--- back/src/api/hospitalDay/hospitalDayController.ts
+++ back/src/api/hospitalDay/hospitalDayController.ts
@@ -20,10 +20,11 @@ export class HospitalDayController {
     public async getHospitalDaysByPage(req: Request, res: Response) {
         const pageNum : string = req.params['page_num'];
         const pack_size : number = 20;
+        const sexe : number = req.params['sexe'] ? Number(req.params['sexe']) : 0
         const start : number = pack_size*(Number(pageNum)-1);
-        const resultat : IHospitalDay[] = await HospitalDayModel.find().skip(start).limit(pack_size);
-        const nextPage : string = `http://localhost:2023/hospitalDay/${+pageNum + 1}`;
-        const prevPage : any = (+pageNum !== 1)? `http://localhost:2023/hospitalDay/${+pageNum - 1}` : null;
+        const resultat : IHospitalDay[] = await HospitalDayModel.find({sexe: sexe}).skip(start).limit(pack_size);
+        const nextPage : string = `http://localhost:2023/hospitalDay/${+pageNum + 1}/${sexe}`;
+        const prevPage : any = (+pageNum !== 1)? `http://localhost:2023/hospitalDay/${+pageNum - 1}/${sexe}` : null;
         let jsonRes : any = {
             nextPage: nextPage,
             prevPage: prevPage,
